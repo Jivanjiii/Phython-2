cd450bcc7e20765256248f1318bbbb9cc03c5d42
jivanjiii <mrmoon827@gmail.com>
2024-03-05 16:39:31 +0300

    first commit

---
+++ patients/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .forms import EmployeeForms
+
+from .forms import StudentForm
+# Create your views here.
+def employee_form(request):
+    form = EmployeeForms()
+    return render(request, 'employee_form.html', {'form':form})
+
+def employee_list(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            form = StudentForm()
+    return render(request, 'employee_list.html', {'form':form})
